027c418e4993bb638b0433ed8d13fe07c3e7740b
rukiyedemirbaysal <rukiyedemirbaysal@gmail.com>
2023-10-06 15:44:21 -0500

    Character management last version

--- queens-gambit-fansite/src/components/CharacterManagement.js
+++ queens-gambit-fansite/src/components/CharacterManagement.js
@@ -36,11 +36,11 @@ const CharacterManagement = () => {
 
   const [editingCharacter, setEditingCharacter] = useState(null);
   const [newCharacter, setNewCharacter] = useState({
-     first_name: '',
-     last_name: '',
+     firstName: '',
+     lastName: '',
      gender:'',
      country: '',
-     FIDE_rating: '', });
+     fideRating: '', });
   const [errors, setErrors] = useState({});
 
   useEffect(() => {
@@ -64,21 +64,42 @@ const CharacterManagement = () => {
     let isValid = true;
     const newErrors = {};
 
-    if (!newCharacter.name.trim()) {
-      newErrors.name = 'Name is required';
+    const trimmedFirstName = newCharacter.name ? newCharacter.name.trim() : '';
+    const trimmedLastName = newCharacter.details ? newCharacter.details.trim() : '';
+    const trimmedGender = newCharacter.details ? newCharacter.details.trim() : '';
+    const trimmedCountry = newCharacter.details ? newCharacter.details.trim() : '';
+    const trimmedFIDErating = newCharacter.details ? newCharacter.details.trim() : '';
+
+    if (!trimmedFirstName) {
+      newErrors.first_name = 'First name is required';
       isValid = false
     }
 
-    if (!newCharacter.details.trim()) {
-      newErrors.details = 'Details are required';
+    if (!trimmedLastName) {
+      newErrors.last_name = 'Last name is required';
       isValid = false;
     }
+
+    if (!trimmedGender) {
+      newErrors.gender = 'Gender is required';
+      isValid = false;
+    }
+    if (!trimmedCountry) {
+      newErrors.country = 'Country is required';
+      isValid = false;
+    }
+    if (!trimmedFIDErating) {
+      newErrors.FIDE_rating = 'FIDE rating is required';
+      isValid = false;
+    }
+
 
     setErrors(newErrors);
     return isValid;
   };
 
   const handleAddCharacter = () => {
+    console.log('Add button clicked');
     if (validateForm()) {
       const method = editingCharacter ? 'PUT' : 'POST';
       const Url = editingCharacter ? `http://localhost:8080/api/characters/${editingCharacter.id}` : 'http://localhost:8080/api/characters'
@@ -95,22 +116,106 @@ const CharacterManagement = () => {
           if (method === 'POST') {
             setCharacters([...characters, data]);
           } else if (method === 'PUT') {
-            setCharacters(characters.map(character =>
-              character.id === editingCharacter.id ? { ...data, ...newCharacter } : character
-            ))
+            setCharacters((characters) =>
+              characters.map((character) =>
+                character.id === editingCharacter.id ? { ...data, ...newCharacter } : character
+              )
+            );
           }
-          setEditingCharacter(null);
-          setNewCharacter({ name: '', details: '' });
+         
+        fetchCharacterData();
           setErrors({});
         })
         .catch(error => console.error('Error making request:', error));
     }
   };
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log('Editing character:', editingCharacter);
+  
+    const method = editingCharacter ? 'PUT' : 'POST';
+    const apiUrl = editingCharacter
+      ? `http://localhost:8080/api/characters/${editingCharacter.characterId}`
+      : 'http://localhost:8080/api/characters';
+  
+    const config = {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newCharacter),
+    };
+  
+    return fetch(apiUrl, config)
+      .then((response) => {
+        if (response.ok) {
+          setNewCharacter({ firstName: '',
+                            lastName: '',
+                            gender: '',
+                            country:'',
+                            fideRating: '', });
+          fetchCharacterData();
+          setErrors({});
+          return response.json();
+        }else {
+          
+        }
+      })
+      .catch((errs) => {
+        console.log('Error response from server:', errs);
+        if (errs.length) {
+          setErrors(errs);
+        } else {
+          setErrors([errs]);
+        }
+      });
+  };
+  
 
   const handleEditCharacter = (character) => {
-    setNewCharacter(character);
+    setNewCharacter({ ...character });
     setEditingCharacter(character);
+
+    if (validateForm()) {
+      const method = 'PUT';
+  
+      const apiUrl = `http://localhost:8080/api/characters/${character.characterId}`;
+  
+      const config = {
+        method,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newCharacter),
+      };
+  
+      fetch(apiUrl, config)
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            return Promise.reject(response.json());
+          }
+        })
+        .then((data) => {
+          const updatedCharacters = characters.map((character) =>
+            character.id === editingCharacter.id ? { ...data, ...newCharacter } : character
+          );
+          setCharacters(updatedCharacters);
+          setEditingCharacter(null);
+          setNewCharacter({ first_name: '', last_name: '', gender: '', country: '', FIDE_rating: '' });
+          fetchCharacterData();
+          setErrors({});
+        })
+        .catch((error) => {
+          console.error('Error making request:', error);
+          setErrors([error]);
+        });
+    }
   };
+  
 
   const handleCancelEdit = () => {
     setEditingCharacter(null);
@@ -118,22 +223,32 @@ const CharacterManagement = () => {
     setErrors({});
   };
 
-
-  const handleDeleteCharacter = (character) => {
-    if (window.confirm('Are you sure you want to delete this character?')) {
-      fetch (`/api/characters/${character.id}` , {
-        method: 'DELETE' , 
-
-      })
-      .then((response) => {
-        if(response.ok) {
-          setCharacters (character.filter((c) => c.id !== character.id));
-      
-        }
-      })
-      .catch((error) => console.error('Error deleting character:', error));
+  const handleDeleteCharacter = async (character) => {
+    const confirmed = window.confirm('Are you sure you want to delete this character?');
+  
+    if (confirmed) {
+      console.log('Character being deleted:', character);
+  
+      if (validateForm()) {
+        const method = 'DELETE';
+        const Url = `http://localhost:8080/api/characters/${character.characterId}` ;
+        fetch(Url, {
+          method,
+        })
+          .then(response => {
+            if (response.ok) {
+              setCharacters(characters.filter(char => char.characterId !== character.characterId));
+              console.log('Character deleted successfully.');
+            } else {
+              console.error('Failed to delete character.');
+            }
+          })
+          .catch(error => console.error('Error making request:', error));
+      }
     }
   };
+  
+  
 
   return (
     <div className="character-management">
@@ -141,7 +256,7 @@ const CharacterManagement = () => {
       <ul className="character-list">
         {characters.map((character) => (
           <li key={character.id}>
-            <strong>Name:</strong> {character.name}<br />
+            <strong>Name:</strong> {character.firstName}<br />
             <span><strong>Details:</strong> {character.details}</span>
             <button onClick={() => handleEditCharacter(character)}>Edit</button>
             <button onClick={() => handleDeleteCharacter(character)}>Delete</button>
@@ -150,36 +265,72 @@ const CharacterManagement = () => {
       </ul>
   
       <h3>{editingCharacter ? 'Edit Character' : 'Add a New Character'}</h3>
+      <form onSubmit={handleSubmit}>
+
       <div className="form-group">
         <input
           type="text"
-          name="name"
-          placeholder="Name"
-          value={newCharacter.name}
+          name="firstName"
+          placeholder="First Name"
+          value={newCharacter.firstName}
           onChange={handleInputChange}
         />
-        {errors.name && <div className="error">{errors.name}</div>}
+        {errors.firstName && <div className="error">{errors.name}</div>}
       </div>
   
       <div className="form-group">
         <input
           type="text"
-          name="details"
-          placeholder="Details"
-          value={newCharacter.details}
+          name="lastName"
+          placeholder="Last Name"
+          value={newCharacter.lastName}
+          onChange={handleInputChange}
+        />
+        {errors.lastName && <div className="error">{errors.details}</div>}
+      </div>
+
+      <div className="form-group">
+        <input
+          type="text"
+          name="gender"
+          placeholder="Gender"
+          value={newCharacter.gender}
+          onChange={handleInputChange}
+        />
+        {errors.details && <div className="error">{errors.details}</div>}
+      </div>
+
+      <div className="form-group">
+        <input
+          type="text"
+          name="country"
+          placeholder="Country"
+          value={newCharacter.country}
+          onChange={handleInputChange}
+        />
+        {errors.details && <div className="error">{errors.details}</div>}
+      </div>
+
+      <div className="form-group">
+        <input
+          type="text"
+          name="fideRating"
+          placeholder="FIDE_rating"
+          value={newCharacter.fideRating}
           onChange={handleInputChange}
         />
         {errors.details && <div className="error">{errors.details}</div>}
       </div>
   
       <ButtonContainer>
-        <button onClick={handleAddCharacter} className="add-button">
+        <button type="submit" className="add-button">
           {editingCharacter ? 'Update Character' : 'Add Character'}
         </button>
         {editingCharacter && (
           <CancelButton onClick={handleCancelEdit}>Cancel</CancelButton>
         )}
       </ButtonContainer>
+      </form>
     </div>
   );
         };
